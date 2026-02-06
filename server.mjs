4af40c43042db45fb8f5641d706600c577c81db3
WANTVOO <voowant4@gmail.com>
2026-02-06 01:33:14 -0500

    initial upload

---
+++ server.mjs
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+// NPC chat route
+app.post("/npc-chat", (req, res) => {
+  const { userText } = req.body;
+  res.json({
+    reply: "NPC heard: " + (userText ?? "")
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Listening on http://localhost:" + PORT);
+});
